718115ea40abdaa8b39b1a5797af8d540a5a1f6d
ImashaPreethini <imashapreethini15@gmail.com>
2026-04-17 13:48:22 +0530

    lab04 q4 final policy class

--- OOPLabQuestions/src/main/java/Lab4/Lab04Main.java
+++ OOPLabQuestions/src/main/java/Lab4/Lab04Main.java
@@ -5,13 +5,9 @@ public class Lab04Main {
       Lecturer Lec = new Lecturer("Nimal silva","L001","COST",3,50000);
       
       LabAssistant LabAssistant1= new LabAssistant("Lahiru","A001","COST",120,500);
-      Lec.displayLecturerDetails();
-      
-      System.out.println("Monthly Payment: "+Lec.calculateMonthlyPayment());
-      System.out.println();
-      
-      LabAssistant1.displayLabAssistantDetails();
-      System.out.println("Monthly Payment: "+LabAssistant1.calculateMonthlyPayment());
-       
-   } 
+      UniversityPolicy.showPolicyHeader();
+      System.out.println(UniversityPolicy.UNIVERSITY_NAME);
+      System.out.println("Bonus of Lecture:"+UniversityPolicy.calculateBonus(Lec.calculateMonthlyPayment()));
+      System.out.println("Bonus of Lab Assistant:"+UniversityPolicy.calculateBonus(LabAssistant1.calculateMonthlyPayment()));
+    } 
 }
