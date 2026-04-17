ae883cb57be2f44609ecae16681077f3f3a7267e
ImashaPreethini <imashapreethini15@gmail.com>
2026-04-17 13:26:15 +0530

    lab04 q3 second child class

--- OOPLabQuestions/src/main/java/Lab4/Lab04Main.java
+++ OOPLabQuestions/src/main/java/Lab4/Lab04Main.java
@@ -4,9 +4,14 @@ public class Lab04Main {
    public static void main(String[] args){
       Lecturer Lec = new Lecturer("Nimal silva","L001","COST",3,50000);
       
+      LabAssistant LabAssistant1= new LabAssistant("Lahiru","A001","COST",120,500);
       Lec.displayLecturerDetails();
       
       System.out.println("Monthly Payment: "+Lec.calculateMonthlyPayment());
+      System.out.println();
       
+      LabAssistant1.displayLabAssistantDetails();
+      System.out.println("Monthly Payment: "+LabAssistant1.calculateMonthlyPayment());
+       
    } 
 }
